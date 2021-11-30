3eccf7bb86d129e4b480e08c374c38d119a201a4
webexpertsdev <mikolaj@renowners.pl>
2021-11-30 16:35:02 +0100

    init

---
+++ swissknife.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Swiss Knife
+ * Plugin URI: http://google.com/
+ * Description: Multi-Plugin for E-Comerce.
+ * Version: 1.0
+ * Author: ourcomapnyname
+ * Author URI: http://google.com/
+ */
