4750d5e1df0c868300bdfdd692efd93620e76970
webexpertsdev <mikolaj@renowners.pl>
2021-12-01 02:30:12 +0100

    delete old files

--- swissknife.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Name: Swiss Knife
- * Plugin URI: http://google.com/
- * Description: Multi-Plugin for E-Comerce.
- * Version: 1.0
- * Author: ourcomapnyname
- * Author URI: http://google.com/
- */
